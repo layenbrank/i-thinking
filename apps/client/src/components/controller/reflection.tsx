dbc04fee2b72e9eddcb6c08e093b1e629dfa9f66
lh <15638470820@163.com>
2025-10-27 20:05:56 +0800

    feat：添加控制器组件及其样式文件

---
+++ apps/client/src/components/controller/reflection.tsx
@@ -0,0 +1,20 @@
+import Bookmark from '@/components/applications/bookmark/index.tsx'
+import Calendar from '@/components/applications/calendar/index.tsx'
+import Developer from '@/components/applications/developer/index.tsx'
+import Example from '@/components/applications/example/index.tsx'
+import Intelligence from '@/components/applications/intelligence/index.tsx'
+import Navigation from '@/components/applications/navigation/index.tsx'
+import Settings from '@/components/applications/settings/index.tsx'
+
+export const Reflection: Readonly<Application.Reflection> = {
+	bookmark: Bookmark,
+	calendar: Calendar,
+	intelligence: Intelligence,
+	settings: Settings,
+	navigation: Navigation,
+	clipchamp: Bookmark,
+	markdown: Bookmark,
+	marketplace: Bookmark,
+	developer: Developer,
+	example: Example
+}
