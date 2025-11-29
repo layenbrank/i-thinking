cbde84269d2348e7a881b25a79df2a13a04cbef4
lh <15638470820@163.com>
2025-11-29 22:07:44 +0800

    chore: 更新 .env.development、manifest.json å?TestComponent.spec.ts 文件

--- apps/extension/src/TestComponent.spec.ts
+++ apps/extension/src/TestComponent.spec.ts
@@ -5,7 +5,11 @@ import TestComponent from './components/Test.vue'
 
 describe('TestComponent', () => {
 	it('renders properly', () => {
-		const wrapper = mount(TestComponent, { props: { msg: 'Hello Vitest' } })
+		const wrapper = mount(TestComponent, {
+			props: {
+				msg: 'Hello Vitest'
+			}
+		})
 		expect(wrapper.text()).toContain('Hello Vitest')
 	})
 })
