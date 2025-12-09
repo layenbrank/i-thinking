299118b0f08c8c8ebb8da3670d513ebee42f06cb
lh <15638470820@163.com>
2025-12-10 00:39:46 +0800

    chore: 更新多个组件、样式和类型定义文件

--- apps/extension/src/components/applications/settings/setting-shape.vue
+++ apps/extension/src/components/applications/settings/setting-shape.vue
@@ -1,11 +1,48 @@
 <script setup lang="ts">
+import { useMirrorStore } from '@/stores/mirror.ts'
+import type { SegmentedProps } from 'ant-design-vue'
+import { isString } from 'lodash-es'
+
 defineOptions({
 	name: 'setting-shape'
 })
+
+const mirrorStore = useMirrorStore()
+
+const options: Mirror.Shape[] = ['circle', 'square', 'rectangle']
+const segmented = computed(function () {
+	const mirror = mirrorStore.mirrors?.find(function (mirror) {
+		return mirror.id === mirrorStore.mirrorID
+	})
+	return mirror?.shape ?? 'square'
+})
+
+function updateSegmented(value: SegmentedProps['value']) {
+	if (!value) return
+	if (!isString(value)) return
+	if (!options.includes(value as Mirror.Shape)) return
+
+	const updateSpec = mirrorStore.mirrors?.find(function (mirror) {
+		return mirror.id === mirrorStore.mirrorID
+	})
+
+	if (!updateSpec) return
+
+	void mirrorStore.toUpdateMirror([
+		{
+			key: updateSpec.id,
+			changes: {
+				shape: value as Mirror.Shape
+			}
+		}
+	])
+}
 </script>
 
 <template>
-	<div class="setting-shape">test</div>
+	<div class="setting-shape">
+		<a-segmented :value="segmented" @update:value="updateSegmented" :options="options" />
+	</div>
 </template>
 
 <style lang="scss" scoped>
--- apps/extension/src/components/applications/settings/setting-size.vue
+++ apps/extension/src/components/applications/settings/setting-size.vue
@@ -1,11 +1,48 @@
 <script setup lang="ts">
+import { useMirrorStore } from '@/stores/mirror.ts'
+import type { SegmentedProps } from 'ant-design-vue'
+import { isString } from 'lodash-es'
+
 defineOptions({
 	name: 'setting-size'
 })
+
+const mirrorStore = useMirrorStore()
+
+const options: Mirror.Size[] = ['mini', 'small', 'medium', 'large', 'huge', 'massive', 'ultra']
+const segmented = computed(function () {
+	const mirror = mirrorStore.mirrors?.find(function (mirror) {
+		return mirror.id === mirrorStore.mirrorID
+	})
+	return mirror?.size ?? 'mini'
+})
+
+function updateSegmented(value: SegmentedProps['value']) {
+	if (!value) return
+	if (!isString(value)) return
+	if (!options.includes(value as Mirror.Size)) return
+
+	const updateSpec = mirrorStore.mirrors?.find(function (mirror) {
+		return mirror.id === mirrorStore.mirrorID
+	})
+
+	if (!updateSpec) return
+
+	void mirrorStore.toUpdateMirror([
+		{
+			key: updateSpec.id,
+			changes: {
+				size: value as Mirror.Size
+			}
+		}
+	])
+}
 </script>
 
 <template>
-	<div class="setting-size">test</div>
+	<div class="setting-size">
+		<a-segmented :value="segmented" @update:value="updateSegmented" :options="options" />
+	</div>
 </template>
 
 <style lang="scss" scoped>
