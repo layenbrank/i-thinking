a48a41f8ff950546e9be0e70e9ff9dba8311717f
lh <15638470820@163.com>
2025-12-24 13:48:54 +0800

    chore: 完善自定义应用表å?

--- apps/client/src/components/applications/marketplace/overlay.tsx
+++ apps/client/src/components/applications/marketplace/overlay.tsx
@@ -1,18 +1,67 @@
-import { Button, Segmented, Card, Form, Input, Flex, type SegmentedProps } from 'antd'
+import {
+	Button,
+	Card,
+	Col,
+	ColorPicker,
+	type ColorPickerProps,
+	Divider,
+	Flex,
+	Form,
+	Input,
+	Radio,
+	Row,
+	Segmented,
+	theme
+} from 'antd'
 import { clsx } from 'clsx'
-import type { SegmentedOptions, SegmentedValue, SegmentedLabeledOption } from 'antd/es/segmented'
-import { BarsOutlined, AppstoreOutlined } from '@ant-design/icons'
+import { cyan, generate, green, presetPalettes, red } from '@ant-design/colors'
+import type { SegmentedLabeledOption, SegmentedOptions } from 'antd/es/segmented'
+import { AppstoreOutlined, BarsOutlined } from '@ant-design/icons'
 
 import Application from '@/components/application/application.tsx'
-import styles from '@/components/applications/marketplace/overlay.module.scss'
+
+type Presets = Required<ColorPickerProps>['presets'][number]
 
 interface Props {
 	visible: boolean
 	onUpdateVisible: (value: boolean) => void
 }
 
+const validateMessages = {
+	required: '${label}是必填的!',
+	types: {
+		title: '${label}不是有效标题!',
+		url: '${label}不是有效链接!'
+	}
+}
+
+function genPresets(presets = presetPalettes) {
+	return Object.entries(presets).map<Presets>(([label, colors]) => ({ label, colors, key: label }))
+}
+
+const customPanelRender: ColorPickerProps['panelRender'] = (
+	_,
+	{ components: { Picker, Presets } }
+) => (
+	<Row
+		justify="space-between"
+		wrap={false}>
+		<Col span={12}>
+			<Presets />
+		</Col>
+		<Divider
+			vertical
+			style={{ height: 'auto' }}
+		/>
+		<Col flex="auto">
+			<Picker />
+		</Col>
+	</Row>
+)
+
 export default function Overlay(props: Props) {
-	const formRef = useRef(null)
+	const { token } = theme.useToken()
+	const [form] = Form.useForm()
 	const [activeSegmented, updateActiveSegment] = useState<SegmentedOptions>()
 	const [segmentedOptions] = useState<SegmentedOptions<SegmentedLabeledOption>>([
 		{
@@ -25,12 +74,18 @@ export default function Overlay(props: Props) {
 		}
 	])
 
-	const [configure, updateConfigure] = useState({
-		title: '',
-		url: ''
+	const presets = genPresets({
+		primary: generate(token.colorPrimary),
+		red,
+		green,
+		cyan
 	})
 
 	function handleEnsure() {}
+
+	function handleFinish(value: any) {
+		console.log('value', value)
+	}
 
 	return (
 		<Application.Overlay
@@ -48,29 +103,68 @@ export default function Overlay(props: Props) {
 					options={segmentedOptions}
 				/>
 				<Card className={clsx(['flex-1 h-full'])}>
-					<Form ref={formRef}>
-						<Input
-							value={configure.title}
-							onInput={(e) =>
-								updateConfigure({
-									title: e.currentTarget.value,
-									url: configure.url
-								})
-							}></Input>
-						<Input
-							value={configure.url}
-							onChange={(e) =>
-								updateConfigure({
-									title: configure.title,
-									url: e.currentTarget.value
-								})
-							}></Input>
+					<Form
+						layout="horizontal"
+						labelCol={{ span: 4 }}
+						wrapperCol={{ span: 20 }}
+						labelAlign="right"
+						form={form}
+						onFinish={handleFinish}
+						validateMessages={validateMessages}
+						initialValues={{
+							layout: 'horizontal'
+						}}
+						style={{
+							maxWidth: 600
+						}}>
+						<Form.Item
+							label="标题"
+							name="title"
+							rules={[{ required: true }]}>
+							<Input placeholder="请输入标题" />
+						</Form.Item>
+						<Form.Item
+							name="url"
+							label="链接"
+							rules={[{ required: true }]}>
+							<Input placeholder="请输入链接" />
+						</Form.Item>
+						<Form.Item
+							name="color"
+							label="背景颜色"
+							rules={[{ required: true }]}>
+							<Radio.Group>
+								<Radio value={1}>A</Radio>
+								<Radio value={2}>B</Radio>
+								<Radio value={3}>C</Radio>
+							</Radio.Group>
+							<ColorPicker
+								defaultValue={token.colorPrimary}
+								styles={{ popupOverlayInner: { width: 480 } }}
+								presets={presets}
+								panelRender={customPanelRender}
+							/>
+						</Form.Item>
+						<Form.Item
+							name="image"
+							label="背景图片"
+							rules={[{ required: true }]}>
+							<Radio.Group
+								options={[
+									{ value: 1, label: 'A' },
+									{ value: 2, label: 'B' },
+									{ value: 3, label: 'C' }
+								]}></Radio.Group>
+						</Form.Item>
+						<Form.Item label={null}>
+							<Button
+								type="primary"
+								htmlType="submit"
+								onClick={handleEnsure}>
+								确认
+							</Button>
+						</Form.Item>
 					</Form>
-					<Button
-						type="primary"
-						onClick={handleEnsure}>
-						确认
-					</Button>
 				</Card>
 			</Flex>
 		</Application.Overlay>
