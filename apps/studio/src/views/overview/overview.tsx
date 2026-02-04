fd0abad35b16481e5453a3a5bc9f7e93ffa08218
lh <15638470820@163.com>
2026-02-04 13:57:39 +0800

    chore: 添加概述视图的样式文件

--- apps/studio/src/views/overview/overview.tsx
+++ apps/studio/src/views/overview/overview.tsx
@@ -1,5 +1,23 @@
 import { clsx } from 'clsx'
 
+import '@/views/overview/overview.scss'
+
 export default function Overview() {
-  return <div className={clsx(['size-full'])}>wao</div>
+  return (
+    <div className={clsx(['size-full'])}>
+      <div
+        id="titlebar"
+        className={clsx(['w-full h-[35px] bg-red-300'])}>
+        <div className="titlebar-drag-region"></div>
+        <div>
+          <span
+            className={clsx(['window-appicon'])}
+            style={{ appRegion: 'drag' } as React.CSSProperties}>
+            11
+          </span>
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  )
 }
