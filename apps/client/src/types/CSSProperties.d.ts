e3dcdbeb4b6e85c8b4c11c46215ef80d19669365
layen <15638470820@163.com>
2026-08-06 19:30:16 +0800

    fix(client): 通过 csstype 增强支持 CSS 自定义属性类型

--- apps/client/src/types/CSSProperties.d.ts
+++ apps/client/src/types/CSSProperties.d.ts
@@ -1,13 +1,31 @@
-declare namespace React {
-  interface CSSProperties {
-    [key: `--${string}`]: string
-    /**
-     * The index signature was removed to enable closed typing for style
-     * using CSSType. You're able to use type assertion or module augmentation
-     * to add properties or an index signature of your own.
-     *
-     * For examples and more information, visit:
-     * https://github.com/frenic/csstype#what-should-i-do-when-i-get-type-errors
-     */
+/**
+ * Allow CSS custom properties on style objects.
+ *
+ * Augment csstype `Properties` (not only React.CSSProperties): empty
+ * `CSSProperties extends Properties` is transparent to excess-property checks,
+ * so custom properties must be declared on `Properties` itself.
+ *
+ * This file must be a module (`import` / `export`) so `declare module`
+ * augments the real `csstype` package instead of replacing it.
+ *
+ * `declare global` keeps the ambient `React` namespace used by `React.FC` etc.
+ *
+ * @see https://github.com/frenic/csstype#what-should-i-do-when-i-get-type-errors
+ */
+import type {} from 'csstype'
+
+declare module 'csstype' {
+  interface Properties {
+    [key: `--${string}`]: string | number | undefined
   }
 }
+
+declare global {
+  namespace React {
+    interface CSSProperties {
+      [key: `--${string}`]: string | number | undefined
+    }
+  }
+}
+
+export {}
